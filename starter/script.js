1ba53e43387064c413ee79a2fa24cd735b4efd00
Owais Ahmad <INFORM.OS221@GMAIL.COM>
2023-02-08 18:45:33 +0530

    SwitchPlayer function

--- starter/script.js
+++ starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current1El = document.getElementById('current--0');
@@ -15,6 +17,18 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0,0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+}
+
 //rolling dice functionality
 btnRoll.addEventListener('click', function() {
 // 1. Generating a random dice roll
@@ -29,12 +43,22 @@ diceEl.src = `dice-${dice}.png`;
 if (dice != 1) {
     //add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 } else {
     //switch to next player
-
-
+   switchPlayer();
 }
-
-
 });
+
+btnHold.addEventListener('click', function(){
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore;
+    document.getElementById(`ciurrent--${activePlayer}`).textContent = scores[activePlayer];
+    
+    // 2. Check if player's score is >= 100
+    // Finish the game
+
+    // Switch to the next Player
+
+})
